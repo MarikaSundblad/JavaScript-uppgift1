f8fb4059de68628a176d17d706e22e1bb655cba6
MarikaSundblad <marika.sundblad89@gmail.com>
2021-02-23 10:59:01 +0100

    validering av namn

--- script.js
+++ script.js
@@ -9,100 +9,59 @@ const btn = document.querySelector('#btn');
 const output = document.querySelector('#output');
 const inputs = document.querySelectorAll('input');
 
+// Validerar förnamn
 const validateFirstName = () => {
+
+  let regEx = /[a-zA-Z]+\D+/;
     
-      if(firstName.value === '') {
-        firstNameError.innerText = 'You have to enter your first name'
-        return false;
+      if(!regEx.test(firstName.value)) {
+        firstNameError.innerText = 'You can not have numbers in your first name'
+        return 0;
       }
       else if (firstName.value.length < 2) {
-        firstNameError.innerText = 'You have to have at least 2 characters'
-        return false;
+        firstNameError.innerText = 'You have to have at least 2 characters in your first name'
+        return 0;
       }
       else {
         firstNameError.innerText = ''
-        return true;
+        return 1;
       }
     }
 
+// Validerar efternamn
 const validateLastName = () => {
+
+  let regEx = /[a-zA-Z]+\D+/;
     
-      if(lastName.value === '') {
-        lastNameError.innerText = 'You have to enter your last name'
-        return false;
+      if(!regEx.test(lastName.value)) {
+        lastNameError.innerText = 'You can not have numbers in your last name'
+        return 0;
       }
       else if (lastName.value.length < 2) {
-        lastNameError.innerText = 'You have to have at least 2 characters'
-        return false;
+        lastNameError.innerText = 'You have to have at least 2 characters in your last name'
+        return 0;
       }
       else {
         lastNameError.innerText = ''
-        return true;
+        return 1;
       }
     }
 
+// Validerar email
 const validateEmail = () => {
 
       let regEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
 
-      if(email.value.match(regEx)) {
+      if(regEx.test(email.value)) {
             emailError.innerText = ''
-            return true;
+            return 1;
       }
       else {
             emailError.innerText = 'You have to put in a valid email'
-            return false;
+            return 0;
       }
 }
-
-
-// Validerar namnen
-// const validateText = (input) => {
-
-//       if(input.value ==='') {
-//             input.classList.add('is-invalid')
-//             return false;      
-//       }
-//       else if(input.value.length < 2) {
-//             input.classList.add('is-invalid');
-//             input.classList.remove('is-valid');
-//             return false;
-//       }
-//       else {
-//             input.classList.add('is-valid');
-//             input.classList.remove('is-invalid');
-//             return true;
-//       }
-// }
-
-// Validerar emailen
-// const validateEmail = (_email) => {
-
-//       let regEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-
-
-//       if(regEx.test(_email.value)) {
-//             _email.classList.add('is-valid');
-//             _email.classList.remove('is-invalid');
-//             return true;
-//       }
-//       else {
-//             _email.classList.add('is-invalid');
-//             _email.classList.remove('is-valid');
-//             return false;
-//       }
-// }
-
-
-// inputs.forEach(input => {
-//       input.addEventListener('click', function(e) {
-//         if(input.type === 'text')
-//           validateText(input);
-//         else if(input.type === 'email')
-//           validateEmail(input);
-//       })
-//     })
 
 // Skapa användare
 const users = []
@@ -114,8 +73,10 @@ class User {
     this.email = email;
     this.id = Date.now().toString()
     this.listener = false;
+    // console.log(this)
   }
 }
+
 
 const createUser = (firstName, lastName, email) => {
   const user = new User(firstName, lastName, email);
@@ -141,7 +102,7 @@ const renderUsers = () => {
 
 const newUser = (user) => {
   let template = `
-    <div class="user animate" id="${user.id}">
+    <div class="user" id="${user.id}">
       <div class="text">
         <h3>${user.firstName} ${user.lastName}</h3>
         <small>${user.email}</small>
@@ -153,17 +114,8 @@ const newUser = (user) => {
 // Submit
 form.addEventListener('submit', (e) => {
   e.preventDefault();
-//   validate('#firstName');
-//   validate('#lastName');
-//   validateEmail(email);
 
-//   if(validateText(firstName) && validateText(lastName) && validateEmail(email)) {
-//     createUser(firstName.value, lastName.value, email.value);
-//     renderUsers();
-//     resetForm();
-//   }
-
-if(validateFirstName(firstName) && validateLastName(lastName) && validateEmail(email)) {
+  if(validateFirstName(firstName) + validateLastName(lastName) + validateEmail(email) === 3) {
       createUser(firstName.value, lastName.value, email.value);
       renderUsers();
       resetForm();
